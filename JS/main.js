6267c5008d8418253bc6b0be9e97074f0e65e976
idkman <tuffestboy@hotmail.com>
2026-04-20 15:30:38 +0200

    clean up

--- JS/main.js
+++ JS/main.js
@@ -1,67 +1,20 @@
 async function fetchData() {
   const response = await fetch("https://smegmastrijder.nl/api/shuffled");
   const data = await response.json();
-  featuredtracks(data.Tracks);
-  featuredartists(data.Artists);
-  featuredtrackbar();
-  featuredartistbar();
+  featured_tracks(data.Tracks);
 }
 
-function featuredtracks(data) {
-  const featuredbar = document.getElementById("featured-tracks");
-  featuredbar.innerHTML = "";
-
-  const carousel = document.createElement("div");
-  carousel.innerHTML = `
-    <div class="slide">
-      <div class="swiper tracks">
-        <div class="swiper-wrapper" id="slider-tracks"></div>
-      </div>
-    </div>
-  `;
-
-  featuredbar.appendChild(carousel);
-
-  const slidesWrapper = document.getElementById("slider-tracks");
+function featured_tracks(data) {
+  const bar = document.getElementById("featured-tracks");
 
   data.forEach(Track => {
-    const slide = document.createElement("div");
-    slide.className = "slide-item swiper-slide";
-    slide.innerHTML = `
-    <div class="art-wrapper featured">
-      <img src="${Track.Artwork}" alt="${Track.Name}" class="artwork card-img">
-    </div>
+    const slideritem = document.createElement("div");
+    slideritem.className = "slider-item";
+    slideritem.innerHTML = `
+      <img src="${Track.Artwork}" alt="${Track.Name}" class="artwork">
     `;
-    slidesWrapper.appendChild(slide);
-  });
-}
 
-function featuredartists(data) {
-  const featuredbar = document.getElementById("featured-artists");
-  featuredbar.innerHTML = "";
-
-  const carousel = document.createElement("div");
-  carousel.innerHTML = `
-    <div class="slide">
-      <div class="swiper artists">
-        <div class="swiper-wrapper" id="slider-artists"></div>
-      </div>
-    </div>
-  `;
-
-  featuredbar.appendChild(carousel);
-
-  const slidesWrapper = document.getElementById("slider-artists");
-
-  data.forEach(Artist => {
-    const slide = document.createElement("div");
-    slide.className = "slide-item swiper-slide";
-    slide.innerHTML = `
-    <div class="art-wrapper featured">
-      <img src="${Artist.Artwork}" alt="${Artist.Name}" class="artwork card-img">
-    </div>
-    `;
-    slidesWrapper.appendChild(slide);
+    bar.appendChild(slideritem);
   });
 }
 
